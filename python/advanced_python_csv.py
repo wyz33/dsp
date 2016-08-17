05678efa6a3ebb9bb2f9397b3bc3192a75d883bc
W Zhang <wanying.zhang@gmail.com>
2016-08-17 16:30:19 -0400

    finished Q6 of advance Python, email.csv included

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+import csv
+# use the csv module to read the csv file
+f = open('faculty.csv')
+reader = csv.reader(f)
+
+#extract all the emails into a list
+header = next(reader)
+emails = []
+email_index = header.index(' email')
+
+for row in reader:
+    emails.append(row[email_index])
+
+#now open a file called emails.csv
+#and write a single email address per line.
+with open("emails.csv", "w") as g:
+    writer = csv.writer(g)
+    for i in emails:    
+        writer.writerow([i])
+        
+g.close()
